604565f66e0d845c20cbb3256cf299f0008a4513
Maciej <m.kolkowsk97@gmail.com>
2024-08-26 17:35:48 +0200

    added initial k82 talos machines, added sandbox machine

---
+++ terraform/proxmox/k8s-talos-cp-1.tf
@@ -0,0 +1,47 @@
+resource "proxmox_vm_qemu" "k8s-talos-cp-1" {
+  name        = "k8s-talos-cp-1"
+  desc  = "talos os control plane"
+  target_node = "pve"
+  vmid  = "200"
+
+  memory  = 2048
+  cores   = 2
+  sockets = 1
+
+  agent = 1
+  onboot = "true"
+  scsihw      = "virtio-scsi-single"
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local-dir-1:iso/talos-v1.7.6-metal-amd64.iso"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = 20
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+    macaddr = "0A:34:79:43:C4:00"
+  }
+
+
+  ### or for a Clone VM operation
+  # clone = "template to clone"
+
+  ### or for a PXE boot VM operation
+  # pxe = true
+  # boot = "scsi0;net0"
+  # agent = 0
+}
